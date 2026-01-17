58eb653879def8f1e334aa73366ee3701c3d557f
mohitul-islam <mohitul449@gmail.com>
2026-01-17 12:20:41 +0600

    fix: docker cmds

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Waiting for MySQL..."
 
-until mysql -h"${DB_HOST}" -P"${DB_PORT:-3306}" -u"${DB_USERNAME}" -p"${DB_PASSWORD}" -e "SELECT 1" >/dev/null 2>&1
+until mysql -h"${DB_HOST}" -P"${DB_PORT:-3306}" -u"${DB_USERNAME}" -p"${DB_PASSWORD}" --skip-ssl -e "SELECT 1" >/dev/null 2>&1
 do
   echo "MySQL not ready... sleeping"
   sleep 2
