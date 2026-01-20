2bda831db0d4f11f50bf9a98915296e2f8bbabdc
mohitul-islam <mohitul449@gmail.com>
2026-01-21 00:58:56 +0600

    fix: make backend alone

--- frontend/src/constants/AppConstants.ts
+++
@@ -1,6 +0,0 @@
-export const AppConstants = {
-    API_URL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/v1',
-    TOKEN_KEY: 'auth_token',
-    REFRESH_TOKEN_KEY: 'refresh_token',
-    USER_KEY: 'user_data',
-};
